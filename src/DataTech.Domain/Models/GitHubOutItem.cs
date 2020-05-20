bc9a589428fb3097460c58dd91e4bfbc2266365e
Michel Balthazar <michel@nfe.io>
2020-05-19 22:35:17 -0300

    refactor GitHubOutItem

--- src/DataTech.Domain/Models/GitHubOutItem.cs
+++ src/DataTech.Domain/Models/GitHubOutItem.cs
@@ -1,6 +1,13 @@
-﻿namespace DataTech.Domain.Models
+﻿using System.Collections.Generic;
+
+namespace DataTech.Domain.Models
 {
     public class GitHubOutItem
+    {
+        public IEnumerable<User> User { get; set; }
+    }
+
+    public class User
     {
         public string Url { get; set; }
         public string Model { get; set; }
